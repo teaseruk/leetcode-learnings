19fa1e8b5ffecf8bef661c004f330d20662dab66
Chris Rawlings <chris.rawlings.mail@gmail.com>
2023-01-29 21:49:44 +0000

    accepted answer

--- golang/two_sum/two_sum.go
+++ golang/two_sum/two_sum.go
@@ -8,47 +8,19 @@ func main() {
 	fmt.Println("Hello")
 }
 
-func twoPower(num uint64) uint64 {
-	var result uint64 = 1
-
-	for power := uint64(1); power <= num; power++ {
-		result *= 2
-	}
-
-	return result
-}
-
 func twoSum(nums []int, target int) []int {
+	lenNums := len(nums)
 
-	if len(nums) < 3 {
-		return []int{0, 1}[:len(nums)]
-	}
+	var matches []int
+	for startIndex := 0; (matches == nil) && (startIndex < lenNums-1); startIndex++ {
+		for secondIndex := startIndex + 1; (matches == nil) && (secondIndex < lenNums); secondIndex++ {
+			sum := nums[startIndex] + nums[secondIndex]
 
-	numPossibilities := twoPower(uint64(len(nums))) - 1
-
-	for i := uint64(1); i <= numPossibilities; i++ {
-		matches := []int{}
-		total := 0
-		hits := 0
-		for expIndex := uint64(0); (hits < 2) && (expIndex < i); expIndex++ {
-			marker := uint64(1) << expIndex
-			bitIsSet := marker&i != 0x0
-
-			if bitIsSet {
-				hits++
-				if hits > 2 {
-					break
-				}
-
-				matches = append(matches, int(expIndex))
-				total += nums[expIndex]
-				if (total == target) && (len(matches) == 2) {
-					return matches
-				}
-
+			if sum == target {
+				matches = []int{startIndex, secondIndex}
 			}
 		}
-
 	}
-	return []int{}
+
+	return matches
 }
