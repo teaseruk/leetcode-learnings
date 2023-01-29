e9f37abd03efac48b6608c928877ae785ffcaa38
Chris Rawlings <chris.rawlings.mail@gmail.com>
2023-01-29 22:12:05 +0000

    next try a map or leave it for now

--- golang/two_sum/two_sum.go
+++ golang/two_sum/two_sum.go
@@ -9,15 +9,13 @@ func main() {
 }
 
 func twoSum(nums []int, target int) []int {
-	lenNums := len(nums)
-
 	var matches []int
-	for startIndex := 0; (matches == nil) && (startIndex < lenNums-1); startIndex++ {
-		for secondIndex := startIndex + 1; (matches == nil) && (secondIndex < lenNums); secondIndex++ {
-			sum := nums[startIndex] + nums[secondIndex]
-
-			if sum == target {
-				matches = []int{startIndex, secondIndex}
+	var copyTarget int
+	for startIndex, startValue := range nums {
+		copyTarget = target - startValue
+		for secondIndex, secondValue := range nums[startIndex+1:] {
+			if secondValue == copyTarget {
+				return []int{startIndex, startIndex + 1 + secondIndex}
 			}
 		}
 	}
